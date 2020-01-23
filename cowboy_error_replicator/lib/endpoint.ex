df78b8541ff706e28d448cabc1f084536457bcdc
chrishop <christopher.hopkins@bbc.co.uk>
2020-01-23 10:34:43 +0000

    added more routes, for better observations

--- cowboy_error_replicator/lib/endpoint.ex
+++ cowboy_error_replicator/lib/endpoint.ex
@@ -10,12 +10,25 @@ defmodule CowboyErrorReplicator.Endpoint do
   plug(:match)
   plug(:dispatch)
 
+
+
   get "/ping" do
     send_resp(conn, 200, "pong!")
   end
 
+  get "/timeout" do
+    Process.sleep(5_000)
+    send_resp(conn, 200, "I should timeout")
+  end
+
   get "/replicate" do
     send_resp(conn, 200, "replicated route reached")
+    send_resp(conn, 200, "replicated route reached (2nd)")
+  end
+
+  get "/with-sleep" do
+    send_resp(conn, 200, "replicated route reached")
+    Process.sleep(3_000)
     send_resp(conn, 200, "replicated route reached (2nd)")
   end
 
